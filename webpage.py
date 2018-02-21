f653db93cd15e719364f052e147961534ca6d953
Joel Barmettler <joel.barmettler@uzh.ch>
2018-02-21 20:35:54 +0100

    Commented Code
    Added some comments about the code to describe what is happening

--- webpage.py
+++ webpage.py
@@ -4,6 +4,7 @@ import shutil
 import os
 import validators
 
+# Basic webpage class that can scrape the html content of an URL
 class Webpage(object):
     def __init__(self, url):
         if not validators.url(url):
@@ -19,6 +20,7 @@ class Webpage(object):
         self._html_content = requests.get(self._url).text
 
 
+# Subclass of Webpage that can also find images in the html content
 class Images_Webpage(Webpage):
 
     _total_images = 0
@@ -30,27 +32,33 @@ class Images_Webpage(Webpage):
     def find_images(self):
         print("Scraping images for {}".format(self._url))
         self.update_html_content()
+        # Find all img tags and get the src link in the html content
         soup = BeautifulSoup(self._html_content, "html.parser")
         for link in soup.find_all("img"):
             img_url = link.get("src")
             if validators.url(img_url):
+                # add new image to list as Online_Image objects and increase class counter
                 print("Found new valid Link: {}".format(img_url))
                 self._images_list.append(Online_Image(img_url))
                 __class__._total_images += 1
 
+    # static method to receive static class variable
     @staticmethod
     def total_images_found():
         return(__class__._total_images)
 
+    # receive back a list of all image links
     def get_images(self):
         return self._images_list
 
+    # Download all images
     def download_all_images(self, folder):
         for img in self._images_list:
             img.download(folder)
 
 
 
+# Class representing an online Image with an url and a name
 class Online_Image(object):
     def __init__(self, url):
         self._url = url
@@ -59,12 +67,14 @@ class Online_Image(object):
     def __str__(self):
         return "Name: {}. Url: {}".format(self._name, self._url)
 
+    # find name as the string part behind the last backslash
     def find_name(self):
         if "/" in self._url:
             last_slash = self._url.rfind("/")
             return self._url[last_slash+1:]
         return self._url
 
+    # download an image to thep rovided folder
     def download(self, folder):
         try:
             img = requests.get(self._url, stream=True)
@@ -76,7 +86,7 @@ class Online_Image(object):
         except:
             print("can not download image - invalid image url: {}".format(self._url))
 
-
+#testing the class 
 if __name__ == "__main__":
     travelblog = Images_Webpage("https://www.justtravelous.com/")
     travelblog.find_images()
