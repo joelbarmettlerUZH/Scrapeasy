7ce40b556f07fd6b718a4aa354d8cd39fdde7cd9
Joel Barmettler <joel.barmettler@uzh.ch>
2018-02-28 00:40:07 +0100

    pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'Scrapeasy',
-  packages = ['Scrapeasy'],
+  name = 'scrapeasy',
+  packages = ['scrapeasy'],
   version = '0.1',
   license='MIT',
   description = 'Scraping in python made easy - receive the content you like in just one line of code!',
@@ -10,5 +10,26 @@ setup(
   url = 'https://github.com/joelbarmettlerUZH/Scrapeasy',
   download_url = 'https://github.com/joelbarmettlerUZH/Scrapeasy/archive/pypi-0_1_1.tar.gz',
   keywords = ['scraping', 'easy', 'scraper', 'website', 'download', 'links', 'images', 'videos'],
-  classifiers = [],
+  classifiers=[
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Scraping',
+
+    # Pick your license as you wish (should match "license" above)
+    'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
