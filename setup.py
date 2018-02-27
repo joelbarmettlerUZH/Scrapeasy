ebb09bbd86c2036fe0389179f20d6bd81478b98e
Joel Barmettler <joel.barmettler@uzh.ch>
2018-02-28 00:45:19 +0100

    pypi

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
   url = 'https://github.com/joelbarmettlerUZH/Scrapeasy',
   download_url = 'https://github.com/joelbarmettlerUZH/Scrapeasy/archive/pypi-0_1_1.tar.gz',
   keywords = ['scraping', 'easy', 'scraper', 'website', 'download', 'links', 'images', 'videos'],
-  classifiers=[
+  classifiers=[  # Optional
     # How mature is this project? Common values are
     #   3 - Alpha
     #   4 - Beta
@@ -19,17 +19,16 @@ setup(
 
     # Indicate who your project is intended for
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: Scraping',
+    'Topic :: Software Development :: Build Tools',
 
-    # Pick your license as you wish (should match "license" above)
+    # Pick your license as you wish
     'License :: OSI Approved :: MIT License',
 
     # Specify the Python versions you support here. In particular, ensure
     # that you indicate whether you support Python 2, Python 3 or both.
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.2',
-    'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
   ],
 )
