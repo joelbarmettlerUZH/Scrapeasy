c0ed376937d322db4ea1abfbdca9e993d282b4c5
Joel Barmettler <joel.barmettler@uzh.ch>
2018-02-28 00:27:11 +0100

    pypi try no 7932874

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 setup(
-  name = 'scrapeasy',
-  packages = ['scrapeasy'],
+  name = 'Scrapeasy',
+  packages = ['Scrapeasy'],
   version = '0.1',
+  license='MIT',
   description = 'Scraping in python made easy - receive the content you like in just one line of code!',
   author = 'Joel Barmettler',
   author_email = 'joel.barmettler@uzh.ch',
   url = 'https://github.com/joelbarmettlerUZH/Scrapeasy',
-  download_url = 'https://github.com/joelbarmettlerUZH/Scrapeasy.git/pypi-release-0.1.tar.gz',
+  download_url = 'https://github.com/joelbarmettlerUZH/Scrapeasy/archive/pypi-0.1.tar.gz',
   keywords = ['scraping', 'easy', 'scraper', 'website', 'download', 'links', 'images', 'videos'],
   classifiers = [],
 )
